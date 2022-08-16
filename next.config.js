dad97bde20d8c7766af391ca8bacb90e888698d0
Rashaun Bennett <99235834+bird556@users.noreply.github.com>
2022-08-16 17:37:55 -0400

    webpack5 true
    future: { webpack5: true }

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  future: { webpack5: true },
   swcMinify: true,
   images: {
     domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
   },
+  
 };
 
 module.exports = nextConfig;
