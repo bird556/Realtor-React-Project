a7f7bbe81c27fa17998f8942ebb2c3c5612d4b11
Rashaun Bennett <99235834+bird556@users.noreply.github.com>
2022-08-16 17:25:26 -0400

    fixed usestate

--- pages/search.js
+++ pages/search.js
@@ -3,36 +3,38 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
-import SearchFilters from '../components/SearchFilters';
+
 import Property from '../components/Property';
-import noresults from '../assets/images/noresults.svg';
-import { fetchApi, baseUrl } from '../utils/fetchApi';
-const search = ({ properties }) => {
+import SearchFilters from '../components/SearchFilters';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+import noresult from '../assets/images/noresult.svg';
+
+const Search = ({ properties }) => {
   const [searchFilters, setSearchFilters] = useState(false);
   const router = useRouter();
 
   return (
     <Box>
       <Flex
+        onClick={() => setSearchFilters(!searchFilters)}
         cursor="pointer"
         bg="gray.100"
         borderBottom="1px"
         borderColor="gray.200"
+        p="2"
         fontWeight="black"
         fontSize="lg"
         justifyContent="center"
         alignItems="center"
-        p="2"
-        onClick={() => setSearchFilters((prevFilters) => !prevFilters)}
       >
-        <Text>Search Property By Filter</Text>
-        <Icon paddingLeft={2} w="7" as={BsFilter} />
+        <Text>Search Property By Filters</Text>
+        <Icon paddingLeft="2" w="7" as={BsFilter} />
       </Flex>
       {searchFilters && <SearchFilters />}
       <Text fontSize="2xl" p="4" fontWeight="bold">
         Properties {router.query.purpose}
       </Text>
-      <Flex flexWrap="wrap" justifyContent="center">
+      <Flex flexWrap="wrap">
         {properties.map((property) => (
           <Property property={property} key={property.id} />
         ))}
@@ -41,14 +43,13 @@ const search = ({ properties }) => {
         <Flex
           justifyContent="center"
           alignItems="center"
-          flexDirection="column"
-          marginTop={5}
+          flexDir="column"
+          marginTop="5"
           marginBottom="5"
         >
-          <Image alt="no result" src={noresults} />
-
-          <Text fontSize="2xl" marginTop="3">
-            No Results Found
+          <Image src={noresult} alt="no results found" />
+          <Text fontSize="xl" marginTop="3">
+            No Result Found.
           </Text>
         </Flex>
       )}
@@ -58,7 +59,7 @@ const search = ({ properties }) => {
 
 export async function getServerSideProps({ query }) {
   const purpose = query.purpose || 'for-rent';
-  const rentFrequency = query.rentFrequency || 'monthly';
+  const rentFrequency = query.rentFrequency || 'yearly';
   const minPrice = query.minPrice || '0';
   const maxPrice = query.maxPrice || '1000000';
   const roomsMin = query.roomsMin || '0';
@@ -79,4 +80,4 @@ export async function getServerSideProps({ query }) {
   };
 }
 
-export default search;
+export default Search;
